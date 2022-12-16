df35d1d388455bf8dc130fae2ee5ff3abc70e754
Luka Jovanovic <luka.jovanovic.1995@gmail.com>
2022-12-16 18:05:12 +0100

    Setup WebSocket Client

--- client/src/graphql/client.js
+++ client/src/graphql/client.js
@@ -1,9 +1,29 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, HttpLink, split } from '@apollo/client';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { createClient } from 'graphql-ws';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { Kind, OperationTypeNode } from 'graphql';
 
-const GRAPHQL_URL = 'http://localhost:9000/graphql';
+const httpLink = new HttpLink({
+  uri: 'http://localhost:9000/graphql',
+});
+
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: 'ws://localhost:9000/graphql',
+  })
+);
+
+const isSubscription = ({ query }) => {
+  const definition = getMainDefinition(query);
+  return (
+    definition.kind === Kind.OPERATION_DEFINITION &&
+    definition.operation === OperationTypeNode.SUBSCRIPTION
+  );
+};
 
 export const client = new ApolloClient({
-  uri: GRAPHQL_URL,
+  link: split(isSubscription, wsLink, httpLink),
   cache: new InMemoryCache(),
 });
 
