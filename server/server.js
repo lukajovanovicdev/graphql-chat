4d9673e10ec243ac2017fd1123d968ab3392cd82
Luka Jovanovic <luka.jovanovic.1995@gmail.com>
2022-12-16 17:51:00 +0100

    Configure WebSocket server

--- server/server.js
+++ server/server.js
@@ -6,23 +6,31 @@ import { readFile } from 'fs/promises';
 import jwt from 'jsonwebtoken';
 import { User } from './db.js';
 import { resolvers } from './resolvers.js';
+import { createServer as createHttpServer } from 'http';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { WebSocketServer } from 'ws';
+import { useServer as useWsServer } from 'graphql-ws/lib/use/ws';
 
 const PORT = 9000;
 const JWT_SECRET = Buffer.from('+Z3zPGXY7v/0MoMm1p8QuHDGGVrhELGd', 'base64');
 
 const app = express();
-app.use(cors(), express.json(), expressjwt({
-  algorithms: ['HS256'],
-  credentialsRequired: false,
-  secret: JWT_SECRET,
-}));
+app.use(
+  cors(),
+  express.json(),
+  expressjwt({
+    algorithms: ['HS256'],
+    credentialsRequired: false,
+    secret: JWT_SECRET,
+  })
+);
 
 app.post('/login', async (req, res) => {
   const { userId, password } = req.body;
   const user = await User.findOne((user) => user.id === userId);
   if (user && user.password === password) {
     const token = jwt.sign({ sub: user.id }, JWT_SECRET);
-    res.json({ token });  
+    res.json({ token });
   } else {
     res.sendStatus(401);
   }
@@ -35,12 +43,18 @@ function getContext({ req }) {
   return {};
 }
 
+const httpServer = createHttpServer(app);
+const wsServer = new WebSocketServer({ server: httpServer, path: '/graphql' });
+
 const typeDefs = await readFile('./schema.graphql', 'utf8');
-const apolloServer = new ApolloServer({ typeDefs, resolvers, context: getContext });
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+useWsServer({ schema }, wsServer);
+
+const apolloServer = new ApolloServer({ schema, resolvers, context: getContext });
 await apolloServer.start();
 apolloServer.applyMiddleware({ app, path: '/graphql' });
 
-app.listen({ port: PORT }, () => {
+httpServer.listen({ port: PORT }, () => {
   console.log(`Server running on port ${PORT}`);
   console.log(`GraphQL endpoint: http://localhost:${PORT}/graphql`);
 });
